dc96c246d0d2197aefde36b262082e9b40d2776e
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2023-06-12 00:23:42 -0300

    progress commit. significant progress on timer loop.

--- sources/library/onStart(1).lua
+++ sources/library/onStart(1).lua
@@ -2,81 +2,109 @@
 --- standard start pieces
 ---  "Michel Vaillancourt <902pe_gaming@wolfstar.ca>"
 
-WS2_Software          = {}
-WS2_Software.id       = "xfr_unit_select_mover"
-WS2_Software.title    = "XFR Unit Select Mover"
-WS2_Software.version  = "0.0.1"
-WS2_Software.created  = "11 Jun 2023 21h05 AST"
-WS2_Software.revision = "11 Jun 2023 22h38 AST"
-WS2_Software.author   = "Michel Vaillancourt <902pe_gaming@wolfstar.ca>"
+WS2_Software               = {}
+WS2_Software.id            = "xfr_unit_select_mover"
+WS2_Software.title         = "XFR Unit Select Mover"
+WS2_Software.version       = "0.0.1"
+WS2_Software.created       = "11 Jun 2023 21h05 AST"
+WS2_Software.revision      = "11 Jun 2023 22h38 AST"
+WS2_Software.author        = "Michel Vaillancourt <902pe_gaming@wolfstar.ca>"
 
 ---
-PrecisionDigits      = 2
-PrecisionValue       = 10 ^ PrecisionDigits
-GramsToKG            = 1000
-Minutes              = {}
-Minutes[1]           = 60
-Minutes[2]           = Minutes[1] * 2
-Minutes[5]           = Minutes[1] * 5
+PrecisionDigits            = 2
+PrecisionValue             = 10 ^ PrecisionDigits
+GramsToKG                  = 1000
+Minutes                    = {}
+Minutes[1]                 = 60
+Minutes[2]                 = Minutes[1] * 2
+Minutes[5]                 = Minutes[1] * 5
 
 ---
-ScreenPulseTable     = {}
-ScreenPulseTable[1]  = "[-=+     ]"
-ScreenPulseTable[2]  = "[ -=+    ]"
-ScreenPulseTable[3]  = "[  -=+   ]"
-ScreenPulseTable[4]  = "[   -=+  ]"
-ScreenPulseTable[5]  = "[    -=+ ]"
-ScreenPulseTable[6]  = "[     -=+]"
-ScreenPulseTable[7]  = "[     -+=]"
-ScreenPulseTable[8]  = "[     +=-]"
-ScreenPulseTable[9]  = "[    +=- ]"
-ScreenPulseTable[10] = "[   +=-  ]"
-ScreenPulseTable[11] = "[  +=-   ]"
-ScreenPulseTable[12] = "[ +=-    ]"
-ScreenPulseTable[13] = "[+=-     ]"
-ScreenPulseTable[14] = "[=+-     ]"
-ScreenPulseTable[15] = "[=-+     ]"
-AnimationPulseIndex  = 1
+ScreenPulseTable           = {}
+ScreenPulseTable[1]        = "[-=+     ]"
+ScreenPulseTable[2]        = "[ -=+    ]"
+ScreenPulseTable[3]        = "[  -=+   ]"
+ScreenPulseTable[4]        = "[   -=+  ]"
+ScreenPulseTable[5]        = "[    -=+ ]"
+ScreenPulseTable[6]        = "[     -=+]"
+ScreenPulseTable[7]        = "[     -+=]"
+ScreenPulseTable[8]        = "[     +=-]"
+ScreenPulseTable[9]        = "[    +=- ]"
+ScreenPulseTable[10]       = "[   +=-  ]"
+ScreenPulseTable[11]       = "[  +=-   ]"
+ScreenPulseTable[12]       = "[ +=-    ]"
+ScreenPulseTable[13]       = "[+=-     ]"
+ScreenPulseTable[14]       = "[=+-     ]"
+ScreenPulseTable[15]       = "[=-+     ]"
+AnimationPulseIndex        = 1
 
 ---
-IndustryStateCode = {}
-IndustryStateCode[1] = {nq = "Stopped", alt = "Stopped", color = ColoursRGB.cyan}
-IndustryStateCode[2] = {nq = "Running", alt = "Waiting", color = ColoursRGB.bluePure}
-IndustryStateCode[3] = {nq = "Jammed missing ingredient", alt = "Jammed", color = ColoursRGB.redPure}
-IndustryStateCode[4] = {nq = "Jammed output full", alt = "Jammed", color = ColoursRGB.redPure}
-IndustryStateCode[5] = {nq = "Jammed no output container", alt = "Jammed", color = ColoursRGB.redPure}
-IndustryStateCode[6] = {nq = "Pending", alt = "Working", color = ColoursRGB.greenPure}
-IndustryStateCode[7] = {nq = "Jammed missing schematics", alt = "Jammed", color = ColoursRGB.redPure}
+IndustryStateCode          = {}
+IndustryStateCode[1]       = { nq = "Stopped", alt = "Stopped", color = ColoursRGB.cyan }
+IndustryStateCode[2]       = { nq = "Running", alt = "Waiting", color = ColoursRGB.bluePure }
+IndustryStateCode[3]       = { nq = "Jammed missing ingredient", alt = "Jammed", color = ColoursRGB.redPure }
+IndustryStateCode[4]       = { nq = "Jammed output full", alt = "Jammed", color = ColoursRGB.redPure }
+IndustryStateCode[5]       = { nq = "Jammed no output container", alt = "Jammed", color = ColoursRGB.redPure }
+IndustryStateCode[6]       = { nq = "Pending", alt = "Working", color = ColoursRGB.greenPure }
+IndustryStateCode[7]       = { nq = "Jammed missing schematics", alt = "Jammed", color = ColoursRGB.redPure }
 
 ---
-RawMaterials = {}
-RawMaterials.T1 = {}
-RawMaterials.T1[1] = {ore = "Bauxite", pure = "Aluminium"}
-RawMaterials.T1[2] = {ore = "Coal", pure = "Carbon"}
-RawMaterials.T1[3] = {ore = "Hematite", pure = "Iron"}
-RawMaterials.T1[4] = {ore = "Quartz", pure = "Silicon"}
+RawMaterials               = {}
+RawMaterials.errorNotFound = "((not found))"
 
-RawMaterials.T2 = {}
-RawMaterials.T2[1] = {ore = "Limestone", pure = "Calcium"}
-RawMaterials.T2[2] = {ore = "Malachite", pure = "Copper"}
+RawMaterials.data = {}
+RawMaterials.data[1]         = { tier = 1, ore_id = 262147665, ore = "Bauxite",   pure_id = 2240749601, pure = "Aluminium" }
+RawMaterials.data[2]         = { tier = 1, ore_id = 299255727, ore = "Coal",      pure_id = 159858782, pure = "Carbon" }
+RawMaterials.data[3]         = { tier = 1, ore_id = 4234772167, ore = "Hematite", pure_id = 198782496, pure = "Iron" }
+RawMaterials.data[4]         = { tier = 1, ore_id = 3724036288, ore = "Quartz",   pure_id = 2589986891, pure = "Silicon" }
 
-RawMaterials.findPure = function (oreName)
-  local pureName = "((not found))"
-  
-  for i = 1, #RawMaterials.T1, 1 do
-    if RawMaterials.T1[i].ore:lower() == oreName:lower() then
-      pureName = RawMaterials.T1[i].pure
+RawMaterials.data[5]         = {  tier = 2, ore_id = 3086347393, ore = "Limestone", pure_id = 2112763718, pure = "Calcium" }
+RawMaterials.data[6]         = {  tier = 2, ore_id = 2289641763, ore = "Malachite", pure_id = 1466453887, pure = "Copper" }
+
+function RawMaterials:findByPureID(pure_id)
+  local rawMaterialData = {self.errorNotFound}
+  for index, record in next, RawMaterials do
+    if record == pure_id then
+      rawMaterialData = {index, record}
     end
   end
 
-  for i = 1, #RawMaterials.T2, 1 do
-    if RawMaterials.T2[i].ore:lower() == oreName:lower() then
-      pureName = RawMaterials.T2[i].pure
-    end
-  end
-
-  return pureName
+  return rawMaterialData
 end
+
+function RawMaterials:findByOreID(ore_id)
+  local rawMaterialData = {self.errorNotFound}
+  for index, record in next, RawMaterials do
+    if record == ore_id then
+      rawMaterialData = {index, record}
+    end
+  end
+
+  return rawMaterialData
+end
+
+function RawMaterials:findByOreName(oreName)
+  local rawMaterialData = {self.errorNotFound}
+  for index, record in next, RawMaterials do
+    if record == oreName then
+      rawMaterialData = {index, record}
+    end
+  end
+
+  return rawMaterialData
+end
+
+function RawMaterials:findByPureNamefunction(pureName)
+  local rawMaterialData = {self.errorNotFound}
+  for index, record in next, RawMaterials do
+    if record.pure == pureName then
+      rawMaterialData = {index, record}
+    end
+  end
+
+  return rawMaterialData
+end
+
 ---
 system.print("\n --------------- \n")
 local msgTitleAndVersion = WS2_Software.title .. "\n" .. WS2_Software.version
--- sources/unit/onTimer(Any).lua
+++ sources/unit/onTimer(Any).lua
@@ -6,6 +6,30 @@ if tag == TimerList[1].name then
     local thisXFRU = XFRUList[i]
     if isUnitAvailable(thisXFRU) then
       thisXFRU.stop(false,false)
+      local currentJobTable = thisXFRU.getInfo()
+
+      local currentProductTable = currentJobTable[13]
+      local currentProductTable = currentJobTable.currentProducts
+      
+      local currentProductID = currentProductTable[1][1]
+      local currentProductID = currentProductTable[1].id
+
+      local currentOre = RawMaterials.findByOreID(currentProductID)
+      local currentPure = ""
+      if currentOre == RawMaterials.errorNotFound then
+        currentPure = RawMaterials.findByPureID(currentProductID)
+      end
+
+      local next_work_id = 0
+      if currentPure == "" then
+        -- we found an ore_id
+        -- find *this* record in the data set, and get that pure_id
+        next_work_id = currentOre.pure_id
+      else
+        -- we are working on a Pure material
+        -- find the *next* record in the data set, and get that ore_id
+        
+      end
     end   
   end
 
